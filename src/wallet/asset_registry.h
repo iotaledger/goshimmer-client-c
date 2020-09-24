a05dcd978dc1384625813a809938d07406e040b6
Sam Chen <sam.chen@iota.org>
2020-09-24 18:26:00 +0800

    Merge asset to asset_registry.

--- src/wallet/asset_registry.h
+++ src/wallet/asset_registry.h
@@ -1,10 +1,24 @@
 #ifndef __WALLET_ASSET_REGISTRY_H__
 #define __WALLET_ASSET_REGISTRY_H__
 
-#include "asset.h"
+#include "address.h"
+#include "balance.h"
+#include "utils/iota_str.h"
+
+/**
+ * @brief Wallet asset represents a container for all the information regarding a colored coin.
+ *
+ */
+typedef struct {
+  byte_t color[BALANCE_COLOR_BYTES]; /**< Color contains the identifier of this asset */
+  iota_str_t* name;                  /**< Name of the asset */
+  iota_str_t* symbol;                /**< currency symbol of the asset (optional) */
+  int precision; /**< Precision defines how many decimal places are shown when showing this asset in wallets */
+  wallet_address_t address; /**< defines the target address where the asset is supposed to be created */
+  uint64_t amount;          /**< the amount of tokens that we want to create */
+} wallet_asset_t;
 
 // hashtable of assets
-
 
 #ifdef __cplusplus
 extern "C" {
