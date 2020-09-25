2304912b1423fdb4ecba5225c700fefad636c3d1
Sam Chen <sam.chen@iota.org>
2020-09-26 00:18:54 +0800

    API: send tx

---
+++ src/client/api/send_transaction.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "client/api/json_utils.h"
+#include "client/api/send_transaction.h"
+#include "client/network/http.h"
+#include "utils/iota_str.h"
+
+// 0 on success
+static int request_builder(byte_t const tx_bytes[], http_buf_t *req) {
+  int ret = 0;
+
+  cJSON *json_root = cJSON_CreateObject();
+  if (json_root == NULL) {
+    return -1;
+  }
+
+  cJSON_AddItemToObject(json_root, "txn_bytes", cJSON_CreateString(tx_bytes));
+  char const *json_text = cJSON_PrintUnformatted(json_root);
+  if (json_text == NULL) {
+    ret = -1;
+  } else {
+    http_buf_append(req, (byte_t *)json_text, strlen(json_text));
+    http_buf2str(req);
+  }
+  cJSON_Delete(json_root);
+
+  return ret;
+}
+
+int send_tx_bytes(tangle_client_conf_t const *conf, byte_t const tx_bytes[], res_send_tx_t *res) {
+  int ret = 0;
+  char const *const cmd_send_tx = "value/sendTransaction";
+  // compose restful api command
+  iota_str_t *cmd = iota_str_new(conf->url);
+  if (cmd == NULL) {
+    printf("[%s:%d]: OOM\n", __func__, __LINE__);
+    return -1;
+  }
+
+  if (iota_str_append(cmd, cmd_send_tx)) {
+    printf("[%s:%d]: string append failed\n", __func__, __LINE__);
+    ret = -1;
+    goto done;
+  }
+
+  // http client configuration
+  http_client_config_t http_conf = {0};
+  http_conf.url = cmd->buf;
+  if (conf->port) {
+    http_conf.port = conf->port;
+  }
+
+  http_buf_t *http_req = http_buf_new();
+  http_buf_t *http_res = http_buf_new();
+  if (http_res == NULL || http_req == NULL) {
+    printf("[%s:%d]: OOM\n", __func__, __LINE__);
+    // TODO
+    ret = -1;
+    goto done;
+  }
+
+  // build request
+  if (request_builder(tx_bytes, http_req) != 0) {
+    printf("[%s:%d]: build request failed\n", __func__, __LINE__);
+    ret = -1;
+    goto done;
+  }
+
+  // send request via http client
+  if (http_client_post(&http_conf, http_req, http_res) != 0) {
+    printf("[%s:%d]: http client post failed\n", __func__, __LINE__);
+    ret = -1;
+    goto done;
+  }
+  http_buf2str(http_res);
+
+  // printf("req: %s\n", http_req->data);
+  // printf("res: %s\n", http_res->data);
+
+  // json deserialization
+  deser_send_tx((char const *const)http_res->data, res);
+
+done:
+  // cleanup command
+  iota_str_destroy(cmd);
+  http_buf_free(http_res);
+  http_buf_free(http_req);
+
+  return ret;
+}
+
+int deser_send_tx(char const *const j_str, res_send_tx_t *res) {
+  char const *const key_id = "transaction_id";
+  int ret = 0;
+  cJSON *json_obj = cJSON_Parse(j_str);
+  if (json_obj == NULL) {
+    return -1;
+  }
+
+  res_err_t *res_err = deser_error(json_obj);
+  if (res_err) {
+    // got an error response
+    printf("[%s:%d] Error response: %s\n", __func__, __LINE__, res_err->msg);
+    return -1;
+  }
+
+  // gets ID
+  if ((ret = json_get_string(json_obj, key_id, res->tx_id, sizeof(res->tx_id))) != 0) {
+    printf("[%s:%d]: gets %s json string failed\n", __func__, __LINE__, key_id);
+    ret = -1;
+    goto end;
+  }
+
+end:
+  cJSON_Delete(json_obj);
+  return ret;
+}
---
+++ src/client/api/send_transaction.h
@@ -0,0 +1,39 @@
+#ifndef __CLIENT_API_SEND_TX_H__
+#define __CLIENT_API_SEND_TX_H__
+
+#include "client/api/response_error.h"
+#include "client/client_service.h"
+#include "core/transation.h"
+
+typedef struct {
+  char tx_id[TRANSACTION_ID_BASE58_LEN];
+} res_send_tx_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Sends a new transaction to the tangle.
+ *
+ * @param[in] conf A client instance
+ * @param[in] tx_bytes A transaction in bytes
+ * @param[out] res A transaction id from response
+ * @return int 0 on success
+ */
+int send_tx_bytes(tangle_client_conf_t const *conf, byte_t const tx_bytes[], res_send_tx_t *res);
+
+/**
+ * @brief Response deserialization
+ *
+ * @param[in] j_str A string of JSON object
+ * @param[out] res A response object
+ * @return int 0 on success
+ */
+int deser_send_tx(char const *const j_str, res_send_tx_t *res);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
